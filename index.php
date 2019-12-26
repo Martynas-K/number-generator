45bc9da44ccb48cf42ce95c851dee3fa6514c9b3
Martynas <martynaskl@gmail.com>
2019-12-26 21:33:54 +0200

    minor update for readability

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ const ITERATIONS = 1000000;
 const MULTIPLIER_A = 16807;
 const MULTIPLIER_B = 48271;
 const DIVIDER = 2147483647; // pow(2, 31) - 1;
+const COMPARISON_VALUE = 0b11111111;
 
 $a = 65;
 $b = 8921;
@@ -28,8 +29,7 @@ function generateNextNumber ($initialValue, $multiplier): int {
  * @return bool
  */
 function checkCondition ($valueA, $valueB): bool {
-    $comparisonValue = 0b11111111;
-    return (($valueA & $comparisonValue) === ($valueB & $comparisonValue)) ? true : false;
+    return (($valueA & COMPARISON_VALUE) === ($valueB & COMPARISON_VALUE)) ? true : false;
 }
 
 // ------ Main function
